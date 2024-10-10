4c70e4460c8bbc9a26702d21007402cbf7efda62
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-10-10 09:22:33 +0700

    create router booking

--- be/src/routes/index.js
+++ be/src/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRouter from "./user.router.js";
 import rolerRouter from "./role.router.js";
+import bookingRouter from './booking.router.js';
 
 const router = express.Router();
 
 // router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 router.use("/users", userRouter);
 router.use("/role", rolerRouter);
+router.use("/booking", bookingRouter);
 
 export default router; 
