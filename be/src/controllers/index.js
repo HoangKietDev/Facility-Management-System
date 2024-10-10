4ae3ddc3838fb7633c462ea07545656716a98ed4
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-10-10 09:05:43 +0700

    thêm mới hàm xử lý controller cho việc booking

--- be/src/controllers/index.js
+++ be/src/controllers/index.js
@@ -1,7 +1,9 @@
 import userController from "./user.controller.js";
 import roleController from "./role.controller.js";
+import bookingController from "./booking.controller.js";
 
 export {
     userController,
     roleController,
+    bookingController,
 }
