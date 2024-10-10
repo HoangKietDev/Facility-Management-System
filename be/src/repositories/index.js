26775258512cc2a63b27eb29b9c549bc2aef2c7b
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-10-10 11:07:13 +0700

    create repositier for booking

--- be/src/repositories/index.js
+++ be/src/repositories/index.js
@@ -1,7 +1,7 @@
 import userRepository from "./user.repository.js";
 import roleRepository from "./role.repository.js";
-
+import bookingRepository from "./booking.repository.js";
 
 export {
-    userRepository, roleRepository, 
+    userRepository, roleRepository, bookingRepository,
 }
