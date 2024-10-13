dc8ab143047cb2ad6f538047330799c0cd3e4a54
Manhpdhe <sapocxdcwqeqweqwe@gmail.com>
2024-10-13 23:43:47 +0700

    fix swagger change version to swagger 2.0 to openapi 3.0.0; and fix bug api booking

--- be/src/controllers/index.js
+++ be/src/controllers/index.js
@@ -1,9 +1,11 @@
 import userController from "./user.controller.js";
 import roleController from "./role.controller.js";
 import bookingController from "./booking.controller.js";
+import notificationController from "./notification.controller.js";
 
 export {
     userController,
     roleController,
     bookingController,
+    notificationController
 }
--- be/src/routes/index.js
+++ be/src/routes/index.js
@@ -2,12 +2,16 @@ import express from 'express';
 import userRouter from "./user.router.js";
 import rolerRouter from "./role.router.js";
 import bookingRouter from './booking.router.js';
+import notificationRouter from './notification.router.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../utils/swagger.json' assert { type: 'json' };
 
 const router = express.Router();
 
-// router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 router.use("/users", userRouter);
 router.use("/role", rolerRouter);
 router.use("/booking", bookingRouter);
+router.use("/notification", notificationRouter);
 
 export default router; 
